dc49a8bda111e92b9479db9a6d9a6fde7da86ea2
Mikko Jokinen <mikko.jokinen@affecto.com>
2016-12-07 10:12:51 +0200

    Change license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject spring-rabbit-clojure "0.1.0"
   :description "A Simple clojure wrapper for Spring AMQP Rabbit"
   :url "https://github.com/Yleisradio/spring-rabbit-clojure"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.springframework.amqp/spring-rabbit "1.6.5.RELEASE"]
                  [cheshire "5.6.3"]]
